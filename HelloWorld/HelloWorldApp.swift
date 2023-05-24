bd204fc345713492df74f7a33d2c32bb44619f48
eolvera <esau.olverahernandez@gmail.com>
2023-05-23 20:59:29 -0600

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Esau David Olvera on 23/05/23.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
